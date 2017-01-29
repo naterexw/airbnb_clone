16eeaa2e0fadbf54e6032e4439e4a2f000878f51
Nate Welling <nate@natewelling.com>
2017-01-29 13:13:26 +0700

    add gem jquery-turbolinks & config application.js for foundation, add _navbar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,9 +11,12 @@
 // about supported directives.
 //
 //= require jquery
+//= require jquery.turbolinks
 //= require jquery_ujs
 //= require foundation
 //= require turbolinks
 //= require_tree .
 
-$(function(){ $(document).foundation(); });
+$(document).on('turbolinks:load', function() {
+  $(function(){ $(document).foundation(); });
+});
