12a0c84ab6b1b14033c684ed446b86ef6ec3ddf6
Nate Welling <nate@natewelling.com>
2017-02-05 14:34:18 +0700

    add Devise :confirmable to Users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
   validates :full_name, presence: true, length: { maximum: 50 }
 
