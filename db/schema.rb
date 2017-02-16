2f737709a4a31eea7f0f56f9765970a8750a7cbc
Nate Welling <nate@natewelling.com>
2017-02-16 14:58:49 +0700

    rails g model Room & migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,32 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170214065108) do
+ActiveRecord::Schema.define(version: 20170216072514) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "rooms", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "home_type"
+    t.string   "room_type"
+    t.integer  "accomodate"
+    t.integer  "bedroom"
+    t.integer  "bathroom"
+    t.string   "listing_name"
+    t.text     "summary"
+    t.string   "address"
+    t.boolean  "is_tv"
+    t.boolean  "is_kitchen"
+    t.boolean  "is_air"
+    t.boolean  "is_heating"
+    t.boolean  "is_internet"
+    t.integer  "price"
+    t.boolean  "active"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.index ["user_id"], name: "index_rooms_on_user_id", using: :btree
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -43,4 +65,5 @@ ActiveRecord::Schema.define(version: 20170214065108) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "rooms", "users"
 end
