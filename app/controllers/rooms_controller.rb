4934c6707f6485ed399c19b7dd3fc07af94d0065
Nate Welling <nate@natewelling.com>
2017-02-17 11:13:48 +0700

    add @photo logic to Room controller for #show, #create, #edit, #update

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -2,6 +2,14 @@ class RoomsController < ApplicationController
   before_action :set_room, only: [:show, :edit, :update]
   before_action :authenticate_user!, except: [:show]
 
+  def index
+    @rooms = current_user.rooms.all
+  end
+
+  def show
+    @photos = @room.photos
+  end
+
   def new
     @room = current_user.rooms.build
   end
@@ -10,25 +18,34 @@ class RoomsController < ApplicationController
     @room = current_user.rooms.build(room_params)
 
     if @room.save
-      redirect_to @room, notice: "Saved..."
+      if params[:images]
+        params[:images].each do |image|
+          @room.photos.create(image: image)
+        end
+      end
+      @photos = @room.photos
+      redirect_to edit_room_path(@room), notice: "Saved..."
     else
       render :new
     end
   end
 
-  def index
-    @rooms = current_user.rooms.all
-  end
-
-  def show
-  end
-
   def edit
+    if current_user.id == @room.user.id
+      @photos = @room.photos
+    else
+      redirect_to root_path, notice: "You don't have permission"
+    end
   end
 
   def update
     if @room.update(room_params)
-      redirect_to @room, notice: "Updated..."
+      if params[:images]
+        params[:images].each do |image|
+          @room.photos.create(image: image)
+        end
+      end
+      redirect_to edit_room_path(@room), notice: "Updated..."
     else
       render :edit
     end
