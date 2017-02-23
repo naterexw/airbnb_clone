2425db8fa45bf9f0f116a3bb95e9b7e3843f1207
Nate Welling <nate@natewelling.com>
2017-02-23 20:22:55 +0700

    fix styling of Rooms#show, remove dependent: :destroy from :rooms in user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :rooms, dependent: :destroy
+  has_many :rooms
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
