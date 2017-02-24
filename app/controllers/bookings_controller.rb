a4e4b63a9cb4828164429b3bf6a81d7c68b6da67
Nate Welling <nate@natewelling.com>
2017-02-24 12:45:06 +0700

    create Booking controller

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,29 @@
+class BookingsController < ApplicationController
+  before_action :set_room
+  before_action :authenticate_user!
+
+  def create
+    @booking = current_user.bookings.build(booking_params)
+
+    if current_user.id == @room.user.id
+      redirect_to @booking.room, flash: { alert: "You can't make booking for your own room" }
+    end
+
+    if @booking.save
+      redirect_to @booking.room, flash: { success: 'Created booking' }
+    else
+      flash[:alert] = @booking.errors.full_messages.first
+      # render :new
+    end
+  end
+
+  private
+
+  def set_room
+    @room = Room.find(params[:room_id])
+  end
+
+  def booking_params
+    params.require(:booking).permit(:room_id, :start_date, :end_date, :price, :total_price)
+  end
+end
